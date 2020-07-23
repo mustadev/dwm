d88da44fee5832725154750da373ee9746d58df8
mustadev <58852293+mustadev@users.noreply.github.com>
2020-07-23 20:17:58 +0100

    change to AZERTY keyboard

--- config.def.h
+++ config.def.h
@@ -84,15 +84,15 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
+	TAGKEYS(                        XK_ampersand,                      0)
+	TAGKEYS(                        XK_eacute,                      1)
+	TAGKEYS(                        XK_quotedbl,                      2)
+	TAGKEYS(                        XK_apostrophe,                      3)
+	TAGKEYS(                        XK_parentleft,                      4)
+	TAGKEYS(                        XK_minus,                      5)
+	TAGKEYS(                        XK_egrave,                      6)
+	TAGKEYS(                        XK_underscore,                      7)
+	TAGKEYS(                        XK_ccedilla,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 };
 
